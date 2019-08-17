924f01d9c725eefafccfb0b241d7b2e9fb128f92
bilalurkmez07 <urkmezbilal07@gmail.com>
2019-08-17 15:41:00 -0500

    "first commit"

---
+++ src/test/java/steps/TrialClass.java
@@ -0,0 +1,34 @@
+package steps;
+
+public class TrialClass {
+
+    public void test(){
+
+    }
+
+    public void test2(){
+
+    }
+
+    public void test3(){
+
+    }
+
+    public void test4(){
+
+    }
+
+    public void test5(){
+
+    }
+
+    public void test6(){
+
+    }
+
+    public void test7(){
+
+    }
+
+
+}
